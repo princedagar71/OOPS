078ce0b49ea114e26f5f42dc5a0d644ecaa847db
Prince <princedagar71@gmail.com>
2021-07-20 14:48:09 +0530

    Class & Method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Person P=new Person();
+        P.name="Prince ";
+        P.age=21;
+        System.out.println(P.name+" "+P.age);
+        P.walk();
+        Person p=new Person();
+        p.name="La";
+        p.age=23;
+        System.out.println(p.name+" "+p.age);
+        p.walk();
+
+
+    }
+}
+class  Person{
+    String name;
+    int age;
+    void walk(){
+        System.out.println("Walking!!!");
     }
 }
